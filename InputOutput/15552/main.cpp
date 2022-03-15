58ba4e65df8b3f8e0fcb2dde79f16c81920741ce
DESKTOP-18I54H5\acorn <acorn0415@naver.com>
2022-03-15 21:07:26 +0900

    upload

--- InputOutput/15552/main.cpp
+++ InputOutput/15552/main.cpp
@@ -3,15 +3,18 @@
 using namespace std;
 
 int main() {
-
-	int N;
-	cin >> N;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int T;
+	cin >> T;
 	vector<int> answer;
 	int A, B;
-	for (int i = 0; i < N; i++) {
-		cin >> A>> B;
+	for (int i = 0; i < T; i++) {
+		cin >> A >> B;
 		answer.push_back(A + B);
-		cout << answer.back()<<"\n";
+		cout << answer.back() << "\n";
 		answer.pop_back();
 	}
+	
 }
+	
